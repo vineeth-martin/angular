b8c108cce9af6c9bb2820d79e7e5be095f15eaaf
vineeth martin <vineethmartinthina@gmail.com>
2021-08-11 22:51:26 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myapp';
+  onSubmit(data: any)
+  {
+    console.warn(data)
+  }
+}
